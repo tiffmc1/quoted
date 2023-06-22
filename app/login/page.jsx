3a69095469f9a56d2b0b31f164dc5cf70de8f273
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-22 15:09:34 -0400

    LOGIN PAGE: imported Login component

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,11 +1,13 @@
 "use client";
 import Link from "next/link";
+import Login from "../components/Auth/Login";
 
 export default function LoginPage() {
 	return (
 		<>
 			<Link href="/">Back</Link>
 			<div>Login</div>
+			<Login />
 		</>
 	);
 }
