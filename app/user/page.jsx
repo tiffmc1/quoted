5b29273cb2bddc5cb550ae3baacabb37f084127a
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-23 18:05:57 -0400

    MAINPAGE: relocated Nav+Footer to layout.js

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -1,13 +1,9 @@
 import Main from "../components/Main";
-import Nav from "../components/NavBar/Nav";
-import Footer from "../components/Footer";
 
 export default function MainPage() {
 	return (
-		<div className="h-screen flex flex-col justify-between">
-			<Nav />
+		<div>
 			<Main />
-			<Footer />
 		</div>
 	);
 }
