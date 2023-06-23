89dfd0525cea6b429f9fd88f30f7d09dc2ddca3b
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-23 00:15:45 -0400

    Login functionality complete

--- app/components/Auth/Login.jsx
+++ app/components/Auth/Login.jsx
@@ -1,43 +1,46 @@
+"use client";
 import { useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import login from "@/app/firebase/auth/login";
 
 export default function Login() {
-	const [loginForm, setLoginForm] = useState({
-		email: "",
-		password: "",
-	});
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const router = useRouter();
 
-	const handleSubmit = () => {
-		console.log("submitted");
-	};
+	const handleSubmit = async (event) => {
+		event.preventDefault();
 
-	const handleChange = (event) => {
-		setLoginForm((prev) => ({
-			...prev,
-			[event.target.name]: event.target.value,
-		}));
+		const { result, error } = await login(email, password);
+
+		if (error) {
+			return console.log("Login Component Error", error);
+		}
+
+		console.log("Login Component Successful", result);
+		return router.push("/main");
 	};
 
 	return (
 		<>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<input
 					required
 					name="email"
 					placeholder="Email"
 					type="email"
-					onChange={handleChange}
+					onChange={(e) => setEmail(e.target.value)}
 				/>
 				<input
 					required
 					name="password"
 					placeholder="Password"
 					type="password"
-					onChange={handleChange}
+					onChange={(e) => setPassword(e.target.value)}
 				/>
 				<button
 					type="submit"
-					onSubmit={handleSubmit}
 					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4"
 				>
 					Log In
