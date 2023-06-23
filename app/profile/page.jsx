5d4ba7ad35d90e9fa461740c00938d665cc6bd5a
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-23 16:05:28 -0400

    PROFILE & QUOTES PAGES created

---
+++ app/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+export default function ProfilePage() {
+	return (
+		<div>
+			<Link href="/main">Back</Link>
+			<div>ProfilePage</div>
+		</div>
+	);
+}
---
+++ app/quotes/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function QuotesPage() {
+	return (
+		<div>
+			<Link href="/main">Back</Link>
+			<div>QuotesPage</div>
+		</div>
+	);
+}
