bcda539e7f1662af34195f007537e7cf1e3ad281
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-23 15:51:25 -0400

    FOOTER: basic styling added

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<div className="h-screen flex justify-center items-end text-sm py-3">
+				{new Date().getFullYear()} &copy; Quoted. All Rights Reserved.
+			</div>
+		</footer>
+	);
 }
