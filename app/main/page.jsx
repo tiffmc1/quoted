2a83de3d950c12a084057e62a630ab1eefc65531
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-23 11:01:18 -0400

    MAIN PAGE: protected to allow only logged in users

--- app/main/page.jsx
+++ app/main/page.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useAuthContext } from "../firebase/context/AuthContext";
+import { useRouter } from "next/navigation";
 
 export default function Main() {
-	return <div>Main Page!</div>;
+	const { user } = useAuthContext();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (!user) router.push("/");
+	}, [user, router]);
+
+	return (
+		<>
+			{user ? (
+				<div>Main Page</div>
+			) : (
+				<div>Only logged in uses can view this page</div>
+			)}
+		</>
+	);
 }
