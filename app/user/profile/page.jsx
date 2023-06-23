77460c2668f6e31dcd17dc4ef5b30b9a3597924b
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-23 18:54:46 -0400

    PROFILEPAGE: dummy data display for styling

--- app/user/profile/page.jsx
+++ app/user/profile/page.jsx
@@ -4,8 +4,13 @@ import Link from "next/link";
 export default function ProfilePage() {
 	return (
 		<div>
-			<Link href="/user">Back</Link>
-			<div>ProfilePage</div>
+			<div className="flex flex-col items-center py-2">
+				<div>Profile Image</div>
+				<div>Account Information</div>
+				<div>Name: Jane Doe</div>
+				<div>Email: testing@email.com</div>
+				<Link href="/user/quotes">Your Quotes</Link>
+			</div>
 		</div>
 	);
 }
