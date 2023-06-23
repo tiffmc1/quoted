2e06955442a11624d02b909bd67b96224c563860
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-23 18:52:49 -0400

    seed: dummy data created for styling *tobedeleted

---
+++ seed.js
@@ -0,0 +1,59 @@
+// quotes data
+let quotesList = [
+	{
+		author: "Walt Disney",
+		timestamp: new Date().getFullYear(),
+		likes: 1,
+		quote: "The way to get started is to quit talking and begin doing.",
+	},
+	{
+		author: "Walt Disney",
+		timestamp: new Date().getFullYear(),
+		likes: 1,
+		quote: "The way to get started is to quit talking and begin doing.",
+	},
+	{
+		author: "Walt Disney",
+		timestamp: new Date().getFullYear(),
+		likes: 1,
+		quote: "The way to get started is to quit talking and begin doing.",
+	},
+	{
+		author: "Walt Disney",
+		timestamp: new Date().getFullYear(),
+		likes: 1,
+		quote: "The way to get started is to quit talking and begin doing.",
+	},
+	{
+		author: "Walt Disney",
+		timestamp: new Date().getFullYear(),
+		likes: 1,
+		quote: "The way to get started is to quit talking and begin doing.",
+	},
+	{
+		author: "Walt Disney",
+		timestamp: new Date().getFullYear(),
+		likes: 1,
+		quote: "The way to get started is to quit talking and begin doing.",
+	},
+	{
+		author: "Walt Disney",
+		timestamp: new Date().getFullYear(),
+		likes: 1,
+		quote: "The way to get started is to quit talking and begin doing.",
+	},
+	{
+		author: "Walt Disney",
+		timestamp: new Date().getFullYear(),
+		likes: 1,
+		quote: "The way to get started is to quit talking and begin doing.",
+	},
+	{
+		author: "Walt Disney",
+		timestamp: new Date().getFullYear(),
+		likes: 1,
+		quote: "The way to get started is to quit talking and begin doing.",
+	},
+];
+
+export { quotesList };
