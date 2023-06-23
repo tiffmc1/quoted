2f438a93dd9504c508415177255498ec51ab2ee4
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-23 16:03:51 -0400

    NAV COM: created w/ styling and imported NavModal

---
+++ app/components/NavBar/Nav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NavModal from "./NavModal";
+
+export default function Nav() {
+	return (
+		<div className="flex flex-row justify-end p-4">
+			<NavModal />
+		</div>
+	);
+}
