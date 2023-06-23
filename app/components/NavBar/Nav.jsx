411e4551fb7019b8d04311559709d06f36f8c4f7
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-23 18:02:36 -0400

    NAVCOM: ternary operator for "back" button added

--- app/components/NavBar/Nav.jsx
+++ app/components/NavBar/Nav.jsx
@@ -1,10 +1,25 @@
+"use client";
 import React from "react";
 import NavModal from "./NavModal";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Nav() {
+	const path = usePathname();
+	const ref = "/user";
+
 	return (
-		<div className="flex flex-row justify-end p-4">
-			<NavModal />
+		<div>
+			{path === ref ? (
+				<div className="flex flex-row justify-end p-4">
+					<NavModal />
+				</div>
+			) : (
+				<div className="flex flex-row justify-between p-4">
+					<Link href="/user">Back</Link>
+					<NavModal />
+				</div>
+			)}
 		</div>
 	);
 }
