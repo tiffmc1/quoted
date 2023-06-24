a365ffa34e77d8384a3fed5cb0fbaa48c6193970
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-24 11:24:05 -0400

    MAINCOM: moved quotesColRef inside useEffect

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,39 +6,40 @@ import { getDocs, collection } from "firebase/firestore";
 export default function Main() {
 	const [quotesList, setQuotesList] = useState([]);
 
-	// const quotesColRef = collection(db, "quotes");
+	useEffect(() => {
+		const getQuotesList = async () => {
+			try {
+				const quotesColRef = collection(db, "quotes");
+				const quotesData = await getDocs(quotesColRef);
+				const filteredQuotes = quotesData.docs.map((doc) => ({
+					...doc.data(),
+					id: doc.id,
+				}));
 
-	// useEffect(() => {
-	// 	const getQuotesList = async () => {
-	// 		try {
-	// 			const data = await getDocs(quotesColRef);
-	// 			const filteredData = data.docs.map((doc) => ({
-	// 				...doc.data(),
-	// 				id: doc.id,
-	// 			}));
-
-	// 			setQuotesList(filteredData);
-	// 		} catch (err) {
-	// 			console.error(err);
-	// 		}
-	// 	};
-	// 	getQuotesList();
-	// }, [quotesColRef]);
+				setQuotesList(filteredQuotes);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+		getQuotesList();
+	}, []);
 
 	return (
-		<div className="quotes-grid">
-			{quotesList.map((quote) => (
-				<div key={quote.id} className="quotes-box">
-					<div>{/* insert user profile pic */}Profile Pic!</div>
-					<div className="text-center">{quote.quote}</div>
-					<div>{quote.author}</div>
-					<div>{quote.likes}</div>
-					<div>
-						{/* insert timestamp & sort from most recent quote to least recent */}
-						Timestamp
+		<div className="p-4">
+			<div className="quotes-grid">
+				{quotesList.map((quote) => (
+					<div key={quote.id} className="quotes-box">
+						<div>{/* insert user profile pic */}Profile Pic!</div>
+						<div className="text-center">{quote.quote}</div>
+						<div>{quote.author}</div>
+						<div>{quote.likes}</div>
+						<div>
+							{/* insert timestamp & sort from most recent quote to least recent */}
+							Timestamp
+						</div>
 					</div>
-				</div>
-			))}
+				))}
+			</div>
 		</div>
 	);
 }
