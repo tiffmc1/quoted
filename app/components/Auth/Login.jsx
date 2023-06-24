48bd320fe54ebeec84d6884ffcbed8b55709e61e
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-24 11:02:32 -0400

    LOGIN: added error message for incorrect pw

--- app/components/Auth/Login.jsx
+++ app/components/Auth/Login.jsx
@@ -7,6 +7,7 @@ import login from "@/app/firebase/auth/login";
 export default function Login() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
 	const router = useRouter();
 
 	const handleSubmit = async (event) => {
@@ -14,8 +15,10 @@ export default function Login() {
 
 		const { result, error } = await login(email, password);
 
+		setErrorMessage("");
 		if (error) {
-			return console.log("Login Component Error", error);
+			setErrorMessage("Incorrect Password. Please Try Again.");
+			return;
 		}
 
 		console.log("Login Component Successful", result);
@@ -24,7 +27,7 @@ export default function Login() {
 
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={handleSubmit} className="flex flex-col">
 				<input
 					required
 					name="email"
@@ -39,6 +42,7 @@ export default function Login() {
 					type="password"
 					onChange={(e) => setPassword(e.target.value)}
 				/>
+				{errorMessage && <div>{errorMessage}</div>}
 				<button
 					type="submit"
 					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4"
