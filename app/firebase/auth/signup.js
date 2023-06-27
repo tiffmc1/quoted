0029e7b807fd907ed77ffa8aa6b4c4524658a2b3
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-27 12:55:31 -0400

    SIGNUP: add user info to db under users collection

--- app/firebase/auth/signup.js
+++ app/firebase/auth/signup.js
@@ -1,16 +1,25 @@
-import { auth } from "../config";
+import { auth, db } from "../config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { addDoc, collection } from "firebase/firestore";
 
-export default async function signUp(email, password, confirmPassword) {
+export default async function signUp(userName, email, password) {
 	let result = null;
 	let error = null;
 
 	try {
-		// if (password !== confirmPassword) {
-		// 	error = "Passwords do not match";
-		// }
+		const createdUser = await createUserWithEmailAndPassword(
+			auth,
+			email,
+			password
+		);
 
-		result = await createUserWithEmailAndPassword(auth, email, password);
+		const user = createdUser.user.uid;
+		console.log("sign up", user);
+		result = await addDoc(collection(db, "users"), {
+			uid: user,
+			email: email,
+			name: userName,
+		});
 	} catch (err) {
 		error = err;
 	}
