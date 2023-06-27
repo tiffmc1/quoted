7f0cab6596db075db27f56e2bd084ba5d0854b48
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-27 12:51:44 -0400

    SIGNUP: users decide their username at this step

--- app/components/Auth/SignUp.jsx
+++ app/components/Auth/SignUp.jsx
@@ -5,28 +5,34 @@ import { useRouter } from "next/navigation";
 import signUp from "@/app/firebase/auth/signup";
 
 export default function SignUp() {
+	const [authorName, setAuthorName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [confirmPassword, setConfirmPassword] = useState("");
-	const [passwordError, setPasswordError] = useState(false);
 	const router = useRouter();
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 
-		const { result, error } = await signUp(email, password, confirmPassword);
+		const { result, error } = await signUp(authorName, email, password);
 
 		if (error) {
-			setPasswordError(true);
+			console.error("Sign Up Error", error);
 		}
 
 		console.log("SignUp Component Successful", result);
-		return router.push("/user");
+		return router.push("/user/profile");
 	};
 
 	return (
 		<>
 			<form onSubmit={handleSubmit}>
+				<input
+					required
+					name="name"
+					placeholder="User/Author Name For Profile"
+					type="text"
+					onChange={(e) => setAuthorName(e.target.value)}
+				/>
 				<input
 					required
 					name="email"
@@ -41,14 +47,13 @@ export default function SignUp() {
 					type="password"
 					onChange={(e) => setPassword(e.target.value)}
 				/>
-				<input
+				{/* <input
 					required
 					name="confirm-password"
 					placeholder="Confirm Password"
 					type="password"
 					onChange={(e) => setConfirmPassword(e.target.value)}
-				/>
-				{passwordError ? <div>Passwords do not match</div> : null}
+				/> */}
 				<button
 					type="submit"
 					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4"
