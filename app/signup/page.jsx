6edf6bc80c0747d84a45f451c62c2b3d9d5a0943
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-27 20:49:33 -0400

    SIGNUPPAGE: removed OAuth component as of now

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import SignUp from "../components/Auth/SignUp";
-import OAuth from "../components/Auth/OAuth";
 
 export default function SignUpPage() {
 	return (
 		<>
 			<Link href="/">Back</Link>
 			<SignUp />
-			<OAuth />
 		</>
 	);
 }
