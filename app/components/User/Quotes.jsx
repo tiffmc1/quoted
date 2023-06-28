cd30a2115d2205bc616bcd8999f6d5902e79319a
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-27 20:49:08 -0400

    QUOTESCOM: consolidated data to reduce excess

--- app/components/User/Quotes.jsx
+++ app/components/User/Quotes.jsx
@@ -1,88 +1,89 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { db } from "@/app/firebase/config";
 import {
-	onSnapshot,
 	collection,
-	query,
-	where,
 	orderBy,
-	addDoc,
-	serverTimestamp,
+	query,
+	onSnapshot,
+	where,
 } from "firebase/firestore";
-import { UserData } from "@/app/firebase/context/AuthContext";
 import Moment from "react-moment";
 import moment from "moment/min/moment-with-locales";
 import "moment-timezone";
+import Image from "next/image";
+import defaultProfileImg from "../../../public/images/profile-img-default.png";
 
-export default function Quotes() {
-	const { user } = UserData();
-	const [userQuotes, setUserQuotes] = useState([]);
-	const [createQuote, setCreateQuote] = useState("");
+export default function Quotes({ user, path }) {
+	const [quotesList, setQuotesList] = useState([]);
 	Moment.globalMoment = moment;
 	Moment.globalFormat = "MM/DD/YYYY hh:mm a";
 
-	console.log("quotes page", user);
-
 	useEffect(() => {
-		if (user) {
-			onSnapshot(
-				query(
-					collection(db, "quotes"),
-					where("uid", "==", user.uid),
-					orderBy("created", "desc")
-				),
-				(snapshot) => {
-					setUserQuotes(
-						snapshot.docs.map((doc) => ({
-							id: doc.id,
-							...doc.data(),
-						}))
-					);
-				}
-			);
-		}
-	}, [user]);
-
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-
-		await addDoc(collection(db, "quotes"), {
-			created: serverTimestamp(),
-			likes: 0,
-			quote: createQuote,
-			uid: user.uid,
-		});
-
-		setCreateQuote("");
-	};
-
-	console.log(userQuotes);
+		const getQuotesList = async () => {
+			if (user && path === "/user/main-feed") {
+				onSnapshot(
+					query(collection(db, "quotes"), orderBy("created", "desc")),
+					(snapshot) => {
+						setQuotesList(
+							snapshot.docs.map((doc) => ({
+								id: doc.id,
+								...doc.data(),
+							}))
+						);
+					}
+				);
+			} else if (user && path === "/user/quotes") {
+				onSnapshot(
+					query(
+						collection(db, "quotes"),
+						where("uid", "==", user.uid),
+						orderBy("created", "desc")
+					),
+					(snapshot) => {
+						setQuotesList(
+							snapshot.docs.map((doc) => ({
+								id: doc.id,
+								...doc.data(),
+							}))
+						);
+					}
+				);
+			}
+		};
+		getQuotesList();
+	}, [user, path]);
 
 	return (
 		<div className="p-4">
 			<div className="quotes-grid">
-				{userQuotes?.map((quote) => (
+				{quotesList.map((quote) => (
 					<div key={quote.id} className="quotes-box">
-						<div>{/* insert user profile pic */}Profile Pic!</div>
-						<div className="text-center">{quote.quote}</div>
-						<div>{user?.name}</div>
-						<div>{quote.likes}</div>
+						<div>
+							{/* {user ? (
+								<Image
+									src={user.profileImg}
+									alt="User profile image"
+									height={100}
+									width={100}
+								/>
+							) : ( */}
+							<Image
+								src={defaultProfileImg}
+								alt="User profile image"
+								height={100}
+								width={100}
+							/>
+							{/* )} */}
+						</div>
+
+						<div className="italic">&quot;{quote.quote}&quot;</div>
+						<div>Author: {user?.name}</div>
 						<Moment>{quote.created?.toDate()}</Moment>
+						<div className="text-right">{quote.likes}</div>
 					</div>
 				))}
 			</div>
-
-			<div>Create Quote</div>
-			<form onSubmit={handleSubmit}>
-				<input
-					required
-					type="text"
-					placeholder="Type Your Quote Here"
-					onChange={(e) => setCreateQuote(e.target.value)}
-				/>
-				<button type="submit">Create</button>
-			</form>
 		</div>
 	);
 }
