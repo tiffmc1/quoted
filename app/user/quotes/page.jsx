118810d0cc48abdc63aacf4d91fb3b610c7deabb
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-27 20:51:13 -0400

    QUOTESPAGE: modularized code for optimization

--- app/user/quotes/page.jsx
+++ app/user/quotes/page.jsx
@@ -1,13 +1,21 @@
+"use client";
 import React from "react";
 import Quotes from "@/app/components/User/Quotes";
+import CreateQuote from "@/app/components/User/CreateQuote";
+import { UserData } from "@/app/firebase/context/AuthContext";
+import { usePathname } from "next/navigation";
 
 export default function QuotesPage() {
+	const { user } = UserData();
+	const path = usePathname();
+
 	return (
 		<>
 			<div className="text-center font-semibold">
-				{/* replace with user's name */}Username&apos;s Quotes History
+				{user?.name}&apos;s Quote History
 			</div>
-			<Quotes />
+			<Quotes user={user} path={path} />
+			<CreateQuote user={user} />
 		</>
 	);
 }
