f46819bcee2983012cd0d6ff863169392421d71f
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-30 11:42:43 -0400

    AUTH PAGES: added MUI icons & some basic styling

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,12 +1,17 @@
 "use client";
 import Link from "next/link";
 import Login from "@/src/components/Auth/Login";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 export default function LoginPage() {
 	return (
 		<>
-			<Link href="/">Back</Link>
-			<div>Login</div>
+			<div className="p-4">
+				<Link href="/">
+					<ArrowBackIosNewIcon />
+				</Link>
+			</div>
+			<div className="font-bold text-2xl text-center py-6">Login</div>
 			<Login />
 		</>
 	);
--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,10 +1,17 @@
+"use client";
 import Link from "next/link";
 import SignUp from "@/src/components/Auth/SignUp";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 export default function SignUpPage() {
 	return (
 		<>
-			<Link href="/">Back</Link>
+			<div className="p-4">
+				<Link href="/">
+					<ArrowBackIosNewIcon />
+				</Link>
+			</div>
+			<div className="font-bold text-2xl text-center py-6">Sign Up</div>
 			<SignUp />
 		</>
 	);
