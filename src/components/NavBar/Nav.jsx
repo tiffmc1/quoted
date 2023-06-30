30d85d8013323c91271396056fc08279549df739
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-30 11:44:05 -0400

    NAV COM: imported MUI icons with styling

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import NavModal from "./NavModal";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import CottageOutlinedIcon from "@mui/icons-material/CottageOutlined";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 
 export default function Nav() {
 	const path = usePathname();
@@ -16,7 +18,14 @@ export default function Nav() {
 				</div>
 			) : (
 				<div className="flex flex-row justify-between p-4">
-					<Link href="/user/main-feed">Home</Link>
+					<Link href="/user/main-feed">
+						<CottageOutlinedIcon />
+					</Link>
+					<div className="absolute left-[50%] translate-x-[-50%]">
+						<Link href="/user/quotes/create">
+							<AddCircleOutlineOutlinedIcon />
+						</Link>
+					</div>
 					<NavModal />
 				</div>
 			)}
