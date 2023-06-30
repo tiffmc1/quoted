e7d40e038ab59f7d1afec0d097025b4c1b248160
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-30 17:15:37 -0400

    HOME PAGE: removed unused imports & styling for buttons (moved styling to global css)

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,9 @@
-"use client";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import Footer from "../components/Footer";
-import { UserData } from "./firebase/context/AuthContext";
-import { useEffect } from "react";
 import logo from "../../public/images/full_logo_transparent_no_buffer.png";
 
 export default function Home() {
-	// const { user } = UserData();
-	// const router = useRouter();
-
-	// console.log("look here", user);
-	// useEffect(() => {
-	// 	if (!user) console.log("you shall not pass");
-	// }, [user]);
-
 	return (
 		<>
 			<div className="h-screen flex flex-col items-center justify-center">
@@ -24,11 +12,11 @@ export default function Home() {
 				</div>
 
 				<div className="basis-1/2 flex flex-col">
-					<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
+					<button>
 						<Link href="/login">Login</Link>
 					</button>
 
-					<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
+					<button>
 						<Link href="/signup" className="">
 							Sign Up
 						</Link>
