0aec9a3398544df4e72f5778338fcd09ab0f457e
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-06-30 13:34:48 -0400

    IMAGE UPLOAD COMPONENT: added some styling to user image & imported MUI icon for when user wants to edit image

--- src/components/User/ImageUpload.jsx
+++ src/components/User/ImageUpload.jsx
@@ -1,7 +1,8 @@
 "use client";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import Image from "next/image";
 import defaultProfileImg from "../../../public/images/profile-img-default.png";
+import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined";
 
 export default function ImageUpload({
 	selectedFile,
@@ -12,35 +13,41 @@ export default function ImageUpload({
 	const selectedFileRef = useRef(null);
 
 	return (
-		<div className="flex flex-col items-center w-full">
-			{selectedFile ? (
-				<>
+		<>
+			<div className="relative flex flex-col items-center w-full">
+				{selectedFile ? (
+					<>
+						<Image
+							src={selectedFile}
+							alt="User profile image"
+							height={150}
+							width={150}
+							className="user-image"
+						/>
+					</>
+				) : (
 					<Image
-						src={selectedFile}
-						alt="User profile image"
-						height={100}
-						width={100}
+						src={defaultProfileImg}
+						alt="Default profile image provided by Quoted"
+						height={150}
+						width={150}
+						className="user-image"
 					/>
-				</>
-			) : (
-				<Image
-					src={defaultProfileImg}
-					alt="Default profile image provided by Quoted"
-					height={100}
-					width={100}
-				/>
-			)}
-			<div>
+				)}
+
 				<input
 					type="file"
 					ref={selectedFileRef}
 					onChange={onSelectImage}
 					hidden
 				/>
-				<button onClick={() => selectedFileRef.current?.click()}>
-					Edit Profile Image
+				<button
+					className="absolute bottom-0 right-0 rounded-full p-2"
+					onClick={() => selectedFileRef.current?.click()}
+				>
+					<AddPhotoAlternateOutlinedIcon fontSize="small" />
 				</button>
 			</div>
-		</div>
+		</>
 	);
 }
